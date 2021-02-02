8e3b5e9b81fbb70aec7fd258a7369df9aa54963f
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-02 22:29:15 +0300

    update string 'welcome user'

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,7 +4,7 @@ from brain_games.cli import welcome_user
 
 def main():
     print("Welcome to the Brain Games!")
-    print(f"Hello, {welcome_user()}")
+    print(f"Hello, {welcome_user()}!")
 
 if __name__ == '__main__':
     main()
