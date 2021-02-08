43b01c2f7c6ef3c29f03f07a99a1aa832bd39f77
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-08 21:43:23 +0300

    add func welcome the game

--- brain_games/kernel.py
+++ brain_games/kernel.py
@@ -4,3 +4,8 @@ Game Engine.
 This module contains functions - interfaces for using and implementing games.
 """
 
+
+def welcome_to_the_game():
+    """Do this print greteeng."""
+    greeteng = 'Welcome to the Brain Games!'
+    print(greeteng)
