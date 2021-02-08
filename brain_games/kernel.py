468cc10073584e26a502d30c179af262f7e3b206
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-08 22:12:11 +0300

    add func game_rules

--- brain_games/kernel.py
+++ brain_games/kernel.py
@@ -9,3 +9,12 @@ def welcome_to_the_game():
     """Do this print greteeng."""
     greeteng = 'Welcome to the Brain Games!'
     print(greeteng)
+
+
+def game_rules(rules: str):
+    """Do this function takes a string of game rules.
+
+    Args:
+        rules: The string
+    """
+    print(rules)
