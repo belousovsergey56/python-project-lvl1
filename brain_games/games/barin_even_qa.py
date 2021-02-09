620cd2cd62bcae8872881b9b80c8ff26ff201adc
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-09 23:18:52 +0300

    create module brain_even_qa, add docstring, import kernel

---
+++ brain_games/games/barin_even_qa.py
@@ -0,0 +1,6 @@
+"""Brain even.
+
+The logic of the game. The player is asked whether the number is even or odd.
+The player wins if the answer is correct three times.
+"""
+from brain_games.kernel import game_rules
