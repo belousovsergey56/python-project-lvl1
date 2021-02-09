ac0ed11642310f1989168b9e1cb8c37e4cdf08a1
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-09 23:36:25 +0300

    add import, refactor docstring, add func brain_even_question, remove kernel import

--- brain_games/games/barin_even_qa.py
+++ brain_games/games/barin_even_qa.py
@@ -1,6 +1,26 @@
 """Brain even.
 
-The logic of the game. The player is asked whether the number is even or odd.
+The methods of the game. The player is asked whether the number is even or odd.
 The player wins if the answer is correct three times.
 """
-from brain_games.kernel import game_rules
+from random import randint
+
+
+def brain_even_rules() -> str:
+    """Brain_even_rules. Returns rules this game.
+
+    Returns:
+        string
+    """
+    return 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def brain_even_question() -> int:
+    """Brain_even_questions.
+
+    Returns random numbers, period 1 to 100.
+
+    Returns:
+        int
+    """
+    return randint(1, 100)
