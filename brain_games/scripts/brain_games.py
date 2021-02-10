9e9d1c25b9e7cc203fb14ad8a539589d73d47780
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-11 01:08:56 +0300

    upd import, add docstring

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-from brain_games.cli import welcome_user
+from cli import welcome_user
 
 
 def main():
+    """Display Greeting."""
     print("Welcome to the Brain Games!")
     print("Hello, {}!".format(welcome_user()))
 
