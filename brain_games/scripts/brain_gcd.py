aa9b812014b98c13f1945c97c41252e35e668312
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-13 18:50:53 +0300

    refactor main function -> remove all, add method run, upd imports

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -7,34 +7,15 @@ from brain_games.games.brain_gcd_qa import (
     brain_game_gcd_question,
     brain_game_gcd_rules,
 )
-from brain_games.kernel import (
-    ask_player_take_answer,
-    check_end_game,
-    welcome_the_game_user,
-)
+from brain_games.kernel import run
 
 
 def main():
-    """Logic game.
-
-    Implementation:
-    1. Welcome the Brain Game
-    2. Game, ask user name
-    3. Hello, user name
-    4. Display rules
-    5. Question for user
-    6. User must give three correct response
-    7. Game over
-
-    """
-    player_name = welcome_the_game_user(brain_game_gcd_rules())
-    for trying in range(3):
-        question = brain_game_gcd_question()
-        answer = ask_player_take_answer(question)
-        correct_answer = brain_game_gcd_correct_answer(question)
-        if check_end_game(trying, answer, correct_answer, player_name):
-            continue
-        break
+    """Game launch."""
+    rules = brain_game_gcd_rules
+    question = brain_game_gcd_question
+    answer = brain_game_gcd_correct_answer
+    run(rules, question, answer)
 
 
 if __name__ == '__main__':
