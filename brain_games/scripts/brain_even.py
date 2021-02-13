08f6b7230dfad521ae6cf8f43ff4cb37ab9c1823
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-13 18:50:45 +0300

    refactor main function -> remove all, add method run, upd imports

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,37 +1,19 @@
 #!/usr/bin/env python
 """Parity check. The Game."""
-from brain_games.kernel import (
-    welcome_the_game_user,
-    ask_player_take_answer,
-    check_end_game,
-)
 from brain_games.games.barin_even_qa import (
-    brain_even_rules,
-    brain_even_question,
     brain_even_check_number,
+    brain_even_question,
+    brain_even_rules,
 )
+from brain_games.kernel import run
 
 
 def main():
-    """Logic game.
-
-    1. Welcome the Brain Game
-    2. Game, ask user name
-    3. Hello, user name
-    4. Display rules
-    5. Question for user
-    6. User must give three correct response
-    7. Game over
-
-    """
-    player_name = welcome_the_game_user(brain_even_rules())
-    for trying in range(3):
-        question = brain_even_question()
-        answer = ask_player_take_answer(question)
-        correct_answer = 'yes' if brain_even_check_number(question) else 'no'
-        if check_end_game(trying, answer, correct_answer, player_name):
-            continue
-        break
+    """Game launch."""
+    rules = brain_even_rules
+    question = brain_even_question
+    answer = brain_even_check_number
+    run(rules, question, answer)
 
 
 if __name__ == '__main__':
