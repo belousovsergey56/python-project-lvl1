5e1127d4ed2c94d24936240b66bc92f506b5b95e
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-13 18:50:30 +0300

    refactor main function -> remove all, add method run, upd imports

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,37 +1,18 @@
 """Calculate the result of the expression. The game."""
-from brain_games.kernel import (
-    check_end_game,
-    welcome_the_game_user,
-    ask_player_take_answer,
-)
 from brain_games.games.brain_calc_qa import (
     brain_calc_check_expression,
-    brain_calc_rules,
     brain_calc_question,
+    brain_calc_rules,
 )
+from brain_games.kernel import run
 
 
 def main():
-    """Logic game.
-
-    Implementation:
-    1. Welcome the Brain Game
-    2. Game, ask user name
-    3. Hello, user name
-    4. Display rules
-    5. Question for user
-    6. User must give three correct response
-    7. Game over
-
-    """
-    player_name = welcome_the_game_user(brain_calc_rules())
-    for trying in range(3):
-        question = brain_calc_question()
-        answer = ask_player_take_answer(question)
-        correct_answer = brain_calc_check_expression(question)
-        if check_end_game(trying, answer, correct_answer, player_name):
-            continue
-        break
+    """Game launch."""
+    rules = brain_calc_rules
+    question = brain_calc_question
+    answer = brain_calc_check_expression
+    run(rules, question, answer)
 
 
 if __name__ == '__main__':
