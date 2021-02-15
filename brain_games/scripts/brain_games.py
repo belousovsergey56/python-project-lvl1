816a7df7ca9c430e0e43e7dbbd9cfbba6fd32e5f
belousovsergey56 <belousovsergej56@gmail.com>
2021-02-15 22:47:55 +0300

    replace print to sys.stdout.writelines

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 """Simple module, with import and function Greeting user."""
+from sys import stdout
+
 from brain_games.cli import welcome_user
 
-WELCOME = 'Welcome to the Brain Games!'
-GREETING = 'Hello, {0}!'
+WELCOME = 'Welcome to the Brain Games!\n'
+GREETING = 'Hello, {0}!\n'
 
 
 def main():
     """Display Greeting."""
-    print(WELCOME)
-    print(GREETING.format(welcome_user()))
+    stdout.writelines(WELCOME)
+    stdout.writelines(GREETING.format(welcome_user()))
 
 
 if __name__ == '__main__':
